1a6ceb3b7e8e7d5e5871b157d2ce3ace3bb2a89d
Linus Duttweiler <dutlinus@swissonline.ch>
2022-06-07 09:54:34 +0200

    PHP Start

--- TBD/src/index.php
+++ TBD/src/index.php
@@ -1,9 +1,16 @@
+<?php 
+require 'db_conn.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+ <head>
     <meta charset="UTF-8">
-    <title>TBD</title>
-</head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="css/style.css">
+/head>
 <body>
 <h1>TBD</h1>
 </body>
