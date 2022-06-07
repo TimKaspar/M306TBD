1fa5d0ef3f1fbdbad9a91cf8cb2f74107e893fa8
Linus Duttweiler <dutlinus@swissonline.ch>
2022-06-07 10:19:49 +0200

    Adding

--- TBD/src/index.php
+++ TBD/src/index.php
@@ -10,8 +10,30 @@ require 'db_conn.php';
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>To-Do List</title>
     <link rel="stylesheet" href="css/style.css">
-/head>
+</head>
 <body>
 <h1>TBD</h1>
+    <div class="main-section">
+       <div class="add-section">
+          <form action="app/add.php" method="POST" autocomplete="off">
+             <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error'){ ?>
+                <input type="text" 
+                     name="title" 
+                     style="border-color: #ff6666"
+                     placeholder="This field is required" />
+              <button type="submit">Add &nbsp; <span>&#43;</span></button>
+
+             <?php }else{ ?>
+              <input type="text" 
+                     name="title" 
+                     placeholder="What do you need to do?" />
+              <button type="submit">Add &nbsp; <span>&#43;</span></button>
+             <?php } ?>
+          </form>
+       </div>
+       <?php 
+          $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+       ?>
+	</div>
 </body>
 </html>
