d1219429379a91b2bf8708154943e0848ca4a4d4
Linus Duttweiler <dutlinus@swissonline.ch>
2022-06-07 10:22:10 +0200

    outpuoutputt

--- TBD/src/index.php
+++ TBD/src/index.php
@@ -34,6 +34,38 @@ require 'db_conn.php';
        <?php 
           $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
        ?>
+       <div class="show-todo-section">
+            <?php if($todos->rowCount() <= 0){ ?>
+                <div class="todo-item">
+                    <div class="empty">
+                        <img src="img/f.png" width="100%" />
+                        <img src="img/Ellipsis.gif" width="80px">
+                    </div>
+                </div>
+            <?php } ?>
+
+            <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+                <div class="todo-item">
+                    <span id="<?php echo $todo['id']; ?>"
+                          class="remove-to-do">x</span>
+                    <?php if($todo['checked']){ ?> 
+                        <input type="checkbox"
+                               class="check-box"
+                               data-todo-id ="<?php echo $todo['id']; ?>"
+                               checked />
+                        <h2 class="checked"><?php echo $todo['title'] ?></h2>
+                    <?php }else { ?>
+                        <input type="checkbox"
+                               data-todo-id ="<?php echo $todo['id']; ?>"
+                               class="check-box" />
+                        <h2><?php echo $todo['title'] ?></h2>
+                    <?php } ?>
+                    <br>
+                    <small>created: <?php echo $todo['date_time'] ?></small> 
+                </div>
+            <?php } ?>
+       </div>
+    </div>
 	</div>
 </body>
 </html>
